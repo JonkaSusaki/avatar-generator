eded242dcf87978f3625db55803d483fd7cc7b41
joaosusaki <jrabalem@gmail.com>
2025-04-17 14:59:07 -0300

    Change readme image

--- examples/example.ts
+++ examples/example.ts
@@ -1,6 +1,6 @@
 import { generateAvatar } from "../dist/index.js";
 import fs from "fs";
 
-const buffer = await generateAvatar("asdasdasdasdasd", 400, 400);
+const buffer = await generateAvatar("8a2c59cb-5174-4110-8798-061c7e65995c", 400, 400);
 
 fs.writeFileSync("./assets/avatar.png", buffer);
